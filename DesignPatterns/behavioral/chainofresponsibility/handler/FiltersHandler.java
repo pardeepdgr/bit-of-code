0d2f8f4e8ed8f2f585e12be09f5cb958b438afb4
pardeepdgr <pardeepdgr@gmail.com>
2016-11-20 20:22:22 +0530

    Add files via upload

---
+++ DesignPatterns/behavioral/chainofresponsibility/handler/FiltersHandler.java
@@ -0,0 +1,27 @@
+package chainofresponsibility.handler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import chainofresponsibility.Chained;
+import chainofresponsibility.bo.Parent;
+
+public class FiltersHandler implements Chained {
+
+	private Chained chain;
+
+	@Override
+	public Map<String, List<String>> load(Parent parent) {
+		Map<String, List<String>> output = new HashMap<String, List<String>>();
+		if (parent.getFilters() != null)
+			output.put("filters", parent.getFilters().getFilter());
+		return output;
+	}
+
+	@Override
+	public void setNextHandler(Chained chain) {
+		this.chain = null;
+	}
+
+}
