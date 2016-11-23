deac133623d09dc9cb0309b8931974000fc10e16
pardeepdgr <pardeepdgr@gmail.com>
2016-11-23 18:52:28 +0530

    23-11-2016

---
+++ Searching/rabin_karp/RabinKarpSubstringSearchTest.java
@@ -0,0 +1,26 @@
+package rabin_karp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RabinKarpSubstringSearchTest {
+
+	@Test
+	public void testStringPatternMatching_WhenPatternMatches() {
+		String text = "aabcbcbbabaabaacbbcca";
+		String pattern = "bbc";
+
+		RabinKarpSubstringSearch rk = new RabinKarpSubstringSearch();
+		assertEquals(16, rk.search(text, pattern));
+	}
+
+	@Test
+	public void testStringPatternMatching_WhenPatternDoesntMatch() {
+		String text = "aabcbcbbabaabaacbbcca";
+		String pattern = "abcd";
+
+		RabinKarpSubstringSearch rk = new RabinKarpSubstringSearch();
+		assertEquals(-1, rk.search(text, pattern));
+	}
+}
