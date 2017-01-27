e37f9ace9769e41a5434d4f0b3957649759770cb
pardeepdgr <pardeepdgr@gmail.com>
2017-01-28 00:17:39 +0530

    Custom Annotations

---
+++ Conceptualization/CustomAnnotation/AnnotationParser.java
@@ -0,0 +1,21 @@
+package CustomAnnotation;
+
+import java.lang.reflect.Method;
+
+public class AnnotationParser {
+
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+		AnnotationParser parser = new AnnotationParser();
+		Method method = parser.getClass().getMethod("parse");
+
+		MultiValueAnnotation annotation = method.getAnnotation(MultiValueAnnotation.class);
+		System.out.println("auotherID: " + annotation.auotherID() 
+				+ " author: " + annotation.author() 
+				+ " reputationPoints: " + annotation.reputationPoints());
+	}
+
+	@MultiValueAnnotation(auotherID = 1, author = "Pardeep", reputationPoints = 10)
+	public void parse() {
+		System.out.println("parsing multi value annotation");
+	}
+}
