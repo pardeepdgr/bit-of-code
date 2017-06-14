f6db365c14b2eecff0d3a6c1301e6e9987cde3dc
Pardeep Dogra <pardeepdgr@gmail.com>
2017-06-14 21:16:44 +0530

    Template Method Design Pattern

---
+++ DesignPatterns/behavioral/templatemethod/WindowsCompiler.java
@@ -0,0 +1,15 @@
+package templatemethod;
+
+public class WindowsCompiler extends CrossCompilerTemplate{
+
+	@Override
+	public void readSourceCode() {
+		System.out.println("Code has been read from Windows platform.");
+	}
+
+	@Override
+	public void compile() {
+		System.out.println("Code has been compiled in Windows platform.");
+	}
+
+}
